d3ee26fac31cd9880c7589c798cae0b8e679fe12
Bayu Sugiantoro <bayusugiantoro5@gmail.com>
2024-06-24 22:41:28 +0700

    singleton

---
+++ singleton_test.go
@@ -0,0 +1,17 @@
+package belajar_golang_logging
+
+import (
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+func TestSingleton(t *testing.T) {
+	logrus.Info("Hello World")
+	logrus.Warn("Hello Warning")
+
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+
+	logrus.Info("Hello World")
+	logrus.Warn("Hello Warning")
+}
